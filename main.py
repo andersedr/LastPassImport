033b1a971a336c96583203419ef3bd6f3757e34a
David Andersen <david.andersen@genmills.com>
2024-04-25 10:12:50 -0500

    Inital Build

---
+++ main.py
@@ -0,0 +1,14 @@
+from lxml import etree
+
+def loadRSS(): 
+    # url of rss feed 
+    main() 
+ 
+def main(): 
+    # load rss from web to update existing xml file 
+    loadRSS() 
+
+if __name__ == "__main__": 
+  
+    # calling main function 
+    main() 

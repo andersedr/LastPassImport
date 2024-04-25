2fdb95dda0a0e97ab179cfedcdaa2852a9abb4ca
David Andersen <david.andersen@genmills.com>
2024-04-25 13:41:24 -0500

    final debug

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ from lxml import etree
 
 def loadFile(): 
     # loadfile
-    doc = etree.parse ( 'TestXML.xml' )
+    return etree.parse ( 'TestXML.xml' )
+
  
 def main(): 
     # Load the file
-    loadFile() 
-
+    file = loadFile() 
+  
     # Update the file
+    stringTag = file.xpath('//String')    
 
-    # Export the file
+    for e in stringTag:
+        key = e.find('Key')
+        if key.text == 'URL':
+            value = e.find('Value')
+            print('URLFound: Update text to "cseo.genmills.com"')
+            value.text = 'cseo.genmills.com'
+            
+    # write the file
+    file.write('testOutput.xml')
+
 
 if __name__ == "__main__": 
   
